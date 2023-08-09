ae6bf65064cd9193f1b45e56041b69fe84420b4a
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-09 18:57:54 +0300

    Add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import ThemeBtn from '@components/ThemeBtn/ThemeBtn';
 
 export default function Header() {
   return (
-    <header>
-      <h1>Where in the world?</h1>
+    <header className="bg-white px-20 py-4 shadow flex justify-between items-center">
+      <h1 className="text-xl font-extrabold">Where in the world?</h1>
       <ThemeBtn />
     </header>
   );
