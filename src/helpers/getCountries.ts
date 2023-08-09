f90f821f33257814b8b99bbc1495ba5f340082f5
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-09 18:56:54 +0300

    Add fetch functions

--- src/helpers/getCountries.ts
+++ src/helpers/getCountries.ts
@@ -1,6 +1,6 @@
 export async function getAllCountries() {
   const res = await fetch(
-    'https://restcountries.com/v3.1/all?fields=name,capital,population,region,capital,flags',
+    'https://restcountries.com/v3.1/all?fields=name,capital,population,region,flags',
     { cache: 'force-cache' }
   );
 
@@ -13,8 +13,20 @@ export async function getAllCountries() {
 
 export async function getCountry(id: string) {
   const res = await fetch(
-    `https://restcountries.com/v3.1/name/${id}?fullText=true`,
+    `https://restcountries.com/v3.1/name/${id}?fullText=true&fields=name,nativeName,population,region,subregion,capital,flags,tld,currencies,languages,borders`,
     { cache: 'force-cache' }
+  );
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  const data = await res.json();
+
+  return data;
+}
+
+export async function getCountriesByCodes(id: string) {
+  const res = await fetch(
+    `https://restcountries.com/v3.1/alpha?codes=${id}&fields=name`
   );
 
   if (!res.ok) throw new Error('Failed to fetch data');
